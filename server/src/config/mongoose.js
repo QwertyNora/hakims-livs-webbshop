fe9d6bcb6ea6cc0c33c4ba09942986e009750207
HampusGileborn <h@gileborn.se>
2024-04-03 10:00:42 +0200

    connectein string

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const databaseURI = process.env.CONNECTION_URI;
+const databaseURI = process.env.MONGODB_CONNECT_URI;
 
 function connectToMongoose() {
   mongoose
     .connect(databaseURI)
     .then(() => {
-      console.log("Connected to db", databaseURI);
+      console.log("Connected to the database successfully.");
     })
     .catch((error) => {
-      console.log("Could not connect to DB", databaseURI, error);
+      console.error("Could not connect to the database. Error:", error);
     });
 }
 
