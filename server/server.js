2a772b19481e4c96778f57d2f6ede15783c64e08
HampusGileborn <h@gileborn.se>
2024-04-03 09:06:53 +0200

    start of branch

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const app = require("./src/app.js");
-// const dotenv = require("dotenv").config();
-
 const { connectToMongoose } = require("./src/config/mongoose.js");
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log("Server running on ", port);
+  console.log(`Server running on port ${port}`);
   connectToMongoose();
 });
+
