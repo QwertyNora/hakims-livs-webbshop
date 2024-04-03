848b2ece13073513307560361057cbee01d8618b
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-03 14:04:09 +0200

    cluster error fixed

--- server/src/controllers/product.Controller.js
+++ server/src/controllers/product.Controller.js
@@ -14,15 +14,31 @@ async function getProducts(req, res) {
 }
 
 async function createProduct(req, res) {
-  try {
-    const _product = req.body;
-    const product = await Product.create(_product);
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
+  console.log("test");
+  if (!req.body) {
+    return res.status(400).json({
+      error: "No body submitted",
     });
   }
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (err) {
+    console.log(err.message);
+    res
+      .status(500)
+      .json({ message: "Error with post request.", error: err.message });
+  }
+  // try {
+  //   const _product = req.body;
+  //   const product = await Product.create(_product);
+  //   res.status(201).json(product);
+  // } catch (error) {
+  //   res.status(404).json({
+  //     message: error.message,
+  //   });
+  // }
 }
 
 module.exports = {
