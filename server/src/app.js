0380ebca3cc74bb87cc0402c173a67f212a114f7
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-04-04 14:24:23 +0200

    deployment cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const Express = require("express");
 const cors = require("cors");
 const productsRouter = require("./routes/product.route");
 
-const uri = process.env.MONGODB_CONNECT_URI;
 const app = Express();
 
 app.use(Express.json());
@@ -12,15 +11,18 @@ app.use(Express.urlencoded({ extended: true }));
 // TODO: Add our origin
 app.use(
   cors({
-    origin: ["http://localhost:3000", uri],
+    origin: [
+      "http://localhost:3000",
+      "https://hakims-livs-webbshop-1.onrender.com",
+    ],
   })
 );
 
 // TODO: Try TEST get req to our database
 
-  app.get("/", (req, res) => {
-    res.send("Hello");
-  });
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 // TODO: ADD OUR ROUTES.
 app.use("/products", productsRouter);
