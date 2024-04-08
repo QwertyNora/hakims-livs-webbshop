2fede2a89e9e003ef2800cd219d2d32f5f5b76ab
HampusGileborn <h@gileborn.se>
2024-04-08 10:04:10 +0200

    category controller update

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -1,1 +1,13 @@
-//TODO: Category controller
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 250,
+  },
+  description: { type: String, trim: true },
+});
