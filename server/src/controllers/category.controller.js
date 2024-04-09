334b119cf3670e65938edd26cd948fa328fc45b0
HampusGileborn <h@gileborn.se>
2024-04-09 13:28:16 +0200

    update

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 250,
-  },
-  description: { type: String, trim: true },
-});
+async function getCategories(req, res) {
+  try {
+    const categories = await categories.findById(req.params.id);
+
+    if (!product) {
+      res.status(404).json({
+        message: "Category not found",
+      });
+    }
+  } catch (error) {
+    onsole.log(err.message);
+    res
+      .status(500)
+      .json({ message: "Error with get request.", error: err.message });
+  }
+}
+
+module.exports = {
+  getCategories
+}
